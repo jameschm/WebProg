a94013e8ce41a71f0632ef3360d7e3be353536cd
jameschm <75088056+jameschm@users.noreply.github.com>
2023-05-05 09:33:11 +0200

    1

---
+++ firstproject/myfirstapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'myfirstapp/index.html')
+
+def formulaire(request):
+    return render(request, 'myfirstapp/formulaire.html')
+
+def main(request):
+    return render(request, 'myfirstapp/main.html')
+
+def bonjour(request):
+    nom=request.GET["nom"]
+    return render(request,'myfirstapp/bonjour.html',{"nom":nom}) 
