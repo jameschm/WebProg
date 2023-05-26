5a5d6a4d53022d3de8d6d5e330cc28539f6cb4b1
jameschm <75088056+jameschm@users.noreply.github.com>
2023-05-26 10:25:18 +0200

    u

--- firstproject/myfirstapp/urls.py
+++ firstproject/myfirstapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('main/', views.main),
     path('ajout/', views.ajout, name='ajout'),
     path('traitement/', views.traitement),
+    path('myfirstapp/affiche/<int:id>/',views.read),
+    path('myfirstapp/update/<int:id>/',views.update),
 ]
--- firstproject/myfirstapp/views.py
+++ firstproject/myfirstapp/views.py
@@ -28,3 +28,28 @@ def ajout(request):
     # Your view logic here
     return render(request, 'myfirstapp/ajout.html')
 
+def read(request, id):
+    # Your logic to retrieve and display the data with the given ID
+    return render(request, 'myfirstapp/read.html')
+
+def affiche(request, id):
+    livre = models.Livre.objects.get(pk=id) 
+    return render(request,"bibliotheque/affiche.html",{"livre": livre})
+
+def update(request, id):
+    # Your view logic here
+    return render(request, 'myfirstapp/update.html')
+
+def traitementupdate(request, id):
+    lform = LivreForm(request.POST)
+    if lform.is_valid():
+        livre = lform.save(commit=False) 
+        livre.id = id; 
+        livre.save() 
+        return HttpResponseRedirect("/bibliotheque/") 
+    else:
+        return render(request, "bibliotheque/update.html", {"form": lform, "id": id})
+
+
+
+
